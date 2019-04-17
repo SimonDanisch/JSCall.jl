684a7cbe683fd01be72184099e4be6987ea335b0
SimonDanisch <sdanisch@gmail.com>
2019-04-17 11:19:00 +0200

    implementation

--- src/JSCall.jl
+++ src/JSCall.jl
@@ -1,5 +1,212 @@
 module JSCall
 
-greet() = print("Hello World!")
+using WebIO, JSExpr
+import WebIO: tojs
+
+const object_pool_identifier = Symbol("window.object_pool")
+
+"""
+References objects stored in Javascript.
+Maps the following expressions to actual calls on the Javascript side:
+```julia
+jso = JSObject(name, scope, typ)
+# getfield:
+x = jso.property # returns a new JSObject
+# setfield
+jso.property = "newval" # Works with JSObjects or Julia objects as newval
+# call:
+result = jso.func(args...) # works with julia objects and other JSObjects as args
+
+# constructors are wrapped this way:
+scene = jso.new.Scene() # same as in JS: scene = new jso.Scene()
+```
+"""
+mutable struct JSObject
+    # fields are private and not accessible via jsobject.field
+    name::Symbol
+    scope::Scope
+    typ::Symbol
+    # transporting the UUID allows us to have a uuid different from the objectid
+    # which will help to better capture === equivalence on the js side.
+    uuid::UInt64
+
+    function JSObject(name::Symbol, scope::Scope, typ::Symbol)
+        obj = new(name, scope, typ)
+        obj.uuid = objectid(obj)
+        finalizer(remove_js_reference, obj)
+        return obj
+    end
+
+    function JSObject(name::Symbol, scope::Scope, typ::Symbol, uuid::UInt64)
+        obj = new(name, scope, typ, uuid)
+        finalizer(remove_js_reference, obj)
+        return obj
+    end
+end
+
+"""
+Removes an JSObject from the object pool!
+"""
+function remove_js_reference(jso::JSObject)
+    evaljs(jso, "delete $(object_pool_identifier)[$(uuid(jso))]")
+end
+
+# define accessors
+for name in (:name, :scope, :typ, :uuid)
+    @eval $(name)(jso::JSObject) = getfield(jso, $(QuoteNode(name)))
+end
+
+"""
+    uuidstr(jso::JSObject)
+
+Returns the uuid as a string
+"""
+uuidstr(jso::JSObject) = string(uuid(jso))
+
+"""
+    JSObject(jso::JSObject, typ::Symbol)
+
+Copy constructor with a new `typ`
+"""
+function JSObject(jso::JSObject, typ::Symbol)
+    jsonew = JSObject(name(jso), scope(jso), typ)
+    # point new object to old one on the javascript side:
+    evaljs(jso, @js $jsonew = $jso)
+    return jsonew
+end
+
+tojsexpr(x::String) = JSString(x)
+tojsexpr(x::JSString) = x
+tojsexpr(x) = error("$(typeof(x)) is not convertible to a JS string / expression")
+
+"""
+    evaljs(
+        jso::JSObject, js::Union{JSString, String};
+        try_fetch = false, try_seconds = 2
+    )
+
+Evaluates Javascript in the scope of JSObject
+"""
+function WebIO.evaljs(
+        jso::JSObject, js::Union{JSString, AbstractString};
+        try_fetch = false, try_seconds = 2
+    )
+    task = evaljs(scope(jso), tojsexpr(js))
+    if try_fetch
+        start = time()
+        while (time() - start) <= try_seconds
+            if istaskdone(task)
+                return fetch(task)
+            end
+        end
+    end
+    return nothing
+end
+
+# Make interpolation work for JSObject
+function WebIO.tojs(jso::JSObject)
+    return @js $(object_pool_identifier)[$(uuidstr(jso))]
+end
+
+"""
+Overloading getproperty to allow the same semantic as Javascript.
+Since there is no `new` keyword in Julia like in JS, we missuse
+jsobject.new, to return an instance of jsobject with a new modifier.
+
+So this Javascript:
+```js
+obj = new Module.Constructor()
+```
+
+Will translates to the following Julia code:
+```Julia
+obj = Module.new.Constructor()
+```
+"""
+function Base.getproperty(jso::JSObject, field::Symbol)
+    if field === :new
+        # Create a new instance of jso, with the `new` modifier
+        return JSObject(jso, :new)
+    else
+        # allocate result object:
+        result = JSObject(field, scope(jso), typ(jso))
+        js = @js begin
+            @var object = $jso
+            @var result = object.$(field)
+            # if result is a class method, we need to bind it to the parent object
+            if result.bind != undefined
+                result = result.bind(object)
+            end
+            $(object_pool_identifier)[$(uuidstr(result))] = result
+        end
+        evaljs(jso, js)
+        return result
+    end
+end
+
+function Base.setproperty!(jso::JSObject, name::Symbol, val)
+    evaljs(jso, @js $(jso).$(name) = $val)
+    return val
+end
+
+modifier(jso::JSObject) = getfield(jso, :typ) === :new ? "new " : ""
+
+"""
+Constructs the arguments for a JS call
+"""
+function get_args(args, kw_args)
+    if isempty(kw_args)
+        return join(tojs.(args), ", ")
+    elseif isempty(args)
+        return tojs(Dict(kw_args))
+    else
+        # TODO: I'm not actually sure about this :D
+        error("""
+        Javascript only supports keyword arguments OR arguments.
+        Found posititional arguments and keyword arguments
+        """)
+    end
+end
+
+"""
+    jsobject(args...; kw_args...)
+
+Call overload for JSObjects.
+Only supports keyword arguments OR positional arguments.
+"""
+function (jso::JSObject)(args...; kw_args...)
+      result = JSObject(:result, scope(jso), :call)
+      input_args = get_args(args, kw_args)
+      js = """
+      var func = $(tojs(jso))
+      var result = $(modifier(jso))func(
+        $input_args
+      )
+      $(object_pool_identifier)[\"$(uuid(result))\"] = result
+      """
+      evaljs(jso, js)
+      return result
+end
+
+
+"""
+    Module(name::Symbol, url::String)
+
+Wraps a Javascript library.
+"""
+function JSModule(name::Symbol, url::String)
+    scope = Scope(imports = [url])
+    mod = JSObject(name, scope, :module)
+    document = JSObject(:document, scope, :module)
+    onimport(scope, @js function (mod)
+        window.object_pool = Dict()
+        window.object_pool[$(uuidstr(mod))] = mod
+        window.object_pool[$(uuidstr(document))] = document
+    end)
+    return mod, document
+end
+
+
+export JSObject, JSModule, scope
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,22 @@
-using JSCall
+using JSCall, WebIO, JSExpr
 using Test
 
-@testset "JSCall.jl" begin
-    # Write your own tests here.
-end
+THREE, document = JSModule(
+    :THREE,
+    "https://cdnjs.cloudflare.com/ajax/libs/three.js/103/three.js",
+)
+(scope(THREE))(dom"div#container"())
+
+scene = THREE.new.Scene()
+width, height = 500, 500
+# Create a basic perspective camera
+camera = THREE.new.PerspectiveCamera(75, width / height, 0.1, 1000)
+camera.position.z = 4
+renderer = THREE.new.WebGLRenderer(Dict(:antialias => true))
+renderer.setClearColor("#ffffff")
+geometry = THREE.new.BoxGeometry(1, 1, 1)
+material = THREE.new.MeshBasicMaterial(color = "#433F81")
+cube = THREE.new.Mesh(geometry, material);
+scene.add(cube)
+container = document.querySelector("#container")
+container.appendChild(renderer.domElement)
